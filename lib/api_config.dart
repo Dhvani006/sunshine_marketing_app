086f02598d9b1b8135707632f8b3a767e3c06e89
Sejal <sejallathigara1008@gmail.com>
2025-08-24 13:33:23 +0530

    ui

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -1,4 +1,4 @@
-const String baseUrl = 'http://10.188.12.66/sunshine_marketing_app_backend';
-const String webSocketUrl = 'ws://10.188.12.66';
+const String baseUrl = 'http://10.127.160.66:8080/sunshine_marketing_app_backend';
+const String webSocketUrl = 'ws://10.127.160.66:8080';
 const String uploadsUrl =
-    'http://10.188.12.66/sunshine_marketing_app_backend/uploads/';
+    'http://10.127.160.66:8080/sunshine_marketing_app_backend/uploads/';
